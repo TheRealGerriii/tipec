b7f4e648fe8f3af4f397c5ee1829351e1a39aee3
TheRealGerriii <hermanyi-gergely@hotmail.com>
2021-12-09 10:02:10 +0100

    Befejezve

--- web/app.js
+++ web/app.js
@@ -1,7 +1,17 @@
+import { User } from './user.js';
 var url = 'https://jsonplaceholder.typicode.com/users/';
+var users = [];
+const ul = document.querySelector('#users');
 fetch(url)
     .then(res => res.json())
-    .then(users => {
-    console.log(users);
+    .then(data => {
+    data.forEach((user) => {
+        let user2 = new User(user.id, user.username, user.email);
+        users.push(user2);
+    });
+    users.forEach((user) => {
+        let li = document.createElement('li');
+        li.textContent = user.username;
+        ul.appendChild(li);
+    });
 });
-export {};
